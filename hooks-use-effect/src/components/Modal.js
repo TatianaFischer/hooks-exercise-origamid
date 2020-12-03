2255480f44b6920793e21bd85e2e698f02eb1995
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-03 15:41:55 -0300

    evento de scroll coloquei return dentro do useEffect para não aparecer mais de um evento de scroll

--- hooks-use-effect/src/components/Modal.js
+++ hooks-use-effect/src/components/Modal.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
 const Modal = () => {
+  React.useEffect(() => {
+    function handleScroll(event) {
+      console.log(event);
+    }
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
   return (
-    <div>
+    <div style={{ height: '100vh' }}>
       <p>Compra realizada com sucesso!</p>
     </div>
   );
