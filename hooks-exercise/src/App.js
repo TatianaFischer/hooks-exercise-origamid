11724f6396e5e10c6201e84160a3c78995b86f7c
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-03 13:03:46 -0300

    Style e JSX

--- hooks-exercise/src/App.js
+++ hooks-exercise/src/App.js
@@ -1,34 +1,22 @@
 import React from 'react';
-import Produto from './Produto';
+import './App.css';
+//exercise - useState:
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
 
 const App = () => {
-  const [dados, setDados] = React.useState(null);
-  const [carregando, setCarregando] = React.useState(null);
-
-  async function handleClick(event) {
-    setCarregando(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
-    );
-    const json = await response.json();
-    setDados(json);
-    setCarregando(false);
-  }
-
   return (
-    <div>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        smartphone
-      </button>
-      <button style={{ margin: '.5rem' }} onClick={handleClick}>
-        tablet
-      </button>
-      {carregando && <p>Carregando...</p>}
-      {!carregando && dados && <Produto dados={dados} />}
-    </div>
+    <section>
+      <button>tablet</button>
+      <button>smartphone</button>
+      <button>notebook</button>
+    </section>
   );
 };
 
