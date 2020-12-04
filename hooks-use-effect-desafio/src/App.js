bb35939a197d97bde9085337b079a7bec20ab712
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-04 16:26:32 -0300

    função de onClick em que altera a preferência do produto ao clicar no botão, pegando o texto do botão como preferência e renderizando

--- hooks-use-effect-desafio/src/App.js
+++ hooks-use-effect-desafio/src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 
 const App = () => {
   const [produto, setProduto] = React.useState(null);
+
+  function handleClick({ target }) {
+    setProduto(target.innerText);
+  }
   return (
     <section>
-      <button>notebook</button>
-      <button>smartphone</button>
+      <h1>Preferência: {produto}</h1>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
     </section>
   );
 };
