fc61968de225a892adf2a6e977ba23d275f58140
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-04 16:52:54 -0300

    uso do fetch para buscar as informações do produto dentro do useEffect

--- hooks-use-effect-desafio/src/components/Produto.js
+++ hooks-use-effect-desafio/src/components/Produto.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const Produto = () => {
-  return <div></div>;
+const Produto = ({ produto }) => {
+  const [dados, setDados] = React.useState(null);
+
+  //para o fetch ja for feito desde o inicio, vai ser feito com useEffect:
+
+  React.useEffect(() => {
+    if (produto !== null)
+      fetch(`https://ranekapi.origamid.dev/json/api/produto/${produto}`)
+        .then((response) => response.json())
+        .then((json) => setDados(json));
+  });
+
+  if (dados === null) return null;
+  return (
+    <section>
+      <h1>{dados.nome}</h1>
+      <p>{dados.preco}</p>
+    </section>
+  );
 };
 
 export default Produto;
