610cb32ffb42c10628d8411cf5d46cc50221e01d
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-05 09:55:33 -0300

    criação do estado dos comentários e do input para adicionar comentários com o onChange para mudar o valor

--- hooks-use-ref/src/App.js
+++ hooks-use-ref/src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <section></section>;
+  const [comentarios, setComentarios] = React.useState(['teste1', 'teste2']);
+
+  const [input, setIput] = React.useState('');
+
+  return (
+    <section>
+      <ul>
+        {comentarios.map((comentario) => (
+          <li key={comentario}>{comentario}</li>
+        ))}
+
+        <input
+          type="text"
+          value={input}
+          onChange={({ target }) => setIput(target.value)}
+        />
+      </ul>
+    </section>
+  );
 };
 
 export default App;
