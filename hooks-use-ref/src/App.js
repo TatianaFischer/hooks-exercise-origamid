d68b58bc2258e76816754af59e918768f6754c03
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-05 10:00:28 -0300

    criação do botão que adiciona novo comentário e a função de adicionar esse comentário handleClick

--- hooks-use-ref/src/App.js
+++ hooks-use-ref/src/App.js
@@ -2,23 +2,26 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  const [comentarios, setComentarios] = React.useState(['teste1', 'teste2']);
+  const [comentarios, setComentarios] = React.useState([]);
 
-  const [input, setIput] = React.useState('');
+  const [input, setInput] = React.useState('');
 
+  const handleClick = () => {
+    setComentarios([...comentarios, input]);
+  };
   return (
     <section>
       <ul>
         {comentarios.map((comentario) => (
           <li key={comentario}>{comentario}</li>
         ))}
-
-        <input
-          type="text"
-          value={input}
-          onChange={({ target }) => setIput(target.value)}
-        />
       </ul>
+      <input
+        type="text"
+        value={input}
+        onChange={({ target }) => setInput(target.value)}
+      />
+      <button onClick={handleClick}>Enviar</button>
     </section>
   );
 };
