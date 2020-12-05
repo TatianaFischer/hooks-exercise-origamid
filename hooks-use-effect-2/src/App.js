717dff79f932a6cc0d1cc055f6b68e541cf332dd
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-05 11:55:51 -0300

    usei useRef para apagar a notificação anterior, caso seja adicionado um novo item, para que não de conflito nas mensagens

--- hooks-use-effect-2/src/App.js
+++ hooks-use-effect-2/src/App.js
@@ -5,11 +5,18 @@ const App = () => {
   const [carrinho, setCarrinho] = React.useState(0);
   const [notificacao, setNotificacao] = React.useState(null);
 
+  //para que a notificação seja apagada depois que adiciona um novo item no carrinho, usaremos useRef. Ou seja, antes de colocar uma mensagem nova dizendo que o item foi adicionado, a notificação anterior será apagada.
+  //achei bem difícil de entender.
+
+  const timeoutRef = React.useRef();
+
   function handleClick() {
     setCarrinho(carrinho + 1);
-
     setNotificacao('Item adicionado ao carrinho');
-    setTimeout(() => {
+
+    clearTimeout(timeoutRef.current);
+
+    timeoutRef.current = setTimeout(() => {
       setNotificacao(null);
     }, 1500);
   }
