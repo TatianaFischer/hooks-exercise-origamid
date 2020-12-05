fd180af2806534fd4df1bf02439dc83e3f9a02d8
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-05 11:49:33 -0300

    botão de adicionar item ao carrinho que acrescenta +1 e aparece na tela quantos itens foram adicionados, notificação avisando que o item foi adicionado que desaparece 1,5s depois

--- hooks-use-effect-2/src/App.js
+++ hooks-use-effect-2/src/App.js
@@ -2,7 +2,25 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <section>teste</section>;
+  const [carrinho, setCarrinho] = React.useState(0);
+  const [notificacao, setNotificacao] = React.useState(null);
+
+  function handleClick() {
+    setCarrinho(carrinho + 1);
+
+    setNotificacao('Item adicionado ao carrinho');
+    setTimeout(() => {
+      setNotificacao(null);
+    }, 1500);
+  }
+
+  return (
+    <section>
+      <p>{notificacao}</p>
+      <button onClick={handleClick}>Adicionar Carrinho</button>
+      <p>Total de itens: {carrinho}</p>
+    </section>
+  );
 };
 
 export default App;
