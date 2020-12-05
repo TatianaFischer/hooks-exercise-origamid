c4a58561612617c538427fa109ff28d0561f239f
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-05 17:50:25 -0300

    exemplo 1 usando useMemo e recuperando o que está no localstorage

--- hooks-use-memo-use-callback/src/App.js
+++ hooks-use-memo-use-callback/src/App.js
@@ -2,7 +2,15 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <section>teste</section>;
+  const [contar, setContar] = React.useState(0);
+  const valor = React.useMemo(() => {
+    const localItem = window.localStorage.getItem('produto');
+    console.log('Aconteceu o memo');
+    return localItem;
+  }, []);
+  console.log(valor);
+
+  return <button onClick={() => setContar(contar + 1)}>{contar}</button>;
 };
 
 export default App;
