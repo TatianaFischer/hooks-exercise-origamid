84db503bdf5bcea91dbc65b8bb4360ba5efe8801
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-06 19:15:52 -0300

    importar no app.js o custom hooks, adicionei os botões e as funções de onClick

--- custom-hooks-1/src/App.js
+++ custom-hooks-1/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import './App.css';
+import useLocalStorage from './hooks/useLocalStorage';
 
 const App = () => {
-  return <section>teste</section>;
+  const [product, setProduct] = useLocalStorage('produto', '');
+
+  function handleClick({ target }) {
+    setProduct(target.innerText);
+  }
+
+  return (
+    <section>
+      <p>Produto preferido:</p>
+      <button onClick={handleClick}>Notebook</button>
+      <button onClick={handleClick}>Smartphone</button>
+    </section>
+  );
 };
 
 export default App;
