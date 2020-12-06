4aa029b5731a0b13984d63f677800e27ea4bf454
tatianafischer <tatiana.fischer@acad.pucrs.br>
2020-12-06 19:09:02 -0300

    custom hooks que verifica se tem os dados no local storage

---
+++ custom-hooks-1/src/hooks/useLocalStorage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const useLocalStorage = (key, initial) => {
+  const [state, setState] = React.useState(() => {
+    const local = window.localStorage.getItem(key);
+
+    return local ? local : initial;
+  });
+
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state);
+  }, [state, key]);
+
+  return [state, setState];
+};
+
+export default useLocalStorage;
