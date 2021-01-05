98774191c143efb3ab5774f06b2121175bbf80d4
tatianafischer <tatiana.fischer@acad.pucrs.br>
2021-01-05 09:03:32 -0300

    useFetch

---
+++ custom-hooks-2/src/hooks/useFetch.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const useFetch = () => {
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [loading, setLoading] = React.useState(null);
+
+  const request = React.useCallback(async (url, options) => {
+    let response;
+    let json;
+    try {
+      setError(null);
+      setLoading(true);
+      response = await fetch(url, options);
+      json = await response.json();
+      if (response.ok === false) throw new Error(json.message);
+    } catch (err) {
+      json = null;
+      setError(err.message);
+    } finally {
+      setData(json);
+      setLoading(false);
+      return { response, json };
+    }
+  }, []);
+
+  return { data, loading, error, request };
+  //não foi retornado como array pq se não sempre tem que desestruturar  na ordem correta
+  //retornando como objeto eu não precisa, da pra puxar somente o que quiser usar.
+};
+
+export default useFetch;
